2d7673bca9f9ede5180e66358cc52ae776038d85
りんご飴196 <kazumac41@gmail.com>
2024-09-20 20:06:23 +0900

    [追加] コマンド

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -1,9 +1,10 @@
 package com.github.ringoame196_s_mcPlugin
 
-import org.bukkit.plugin.java.JavaPlugin
 import com.github.ringoame196_s_mcPlugin.commands.Command
 import com.github.ringoame196_s_mcPlugin.commands.TabCompleter
 import com.github.ringoame196_s_mcPlugin.events.Events
+import com.github.ringoame196_s_mcPlugin.managers.BackupManager
+import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
     override fun onEnable() {
@@ -13,8 +14,10 @@ class Main : JavaPlugin() {
         saveDefaultConfig() // configファイル生成
         backupManager.makeBackupFolder() // バックアップフォルダー作成
         server.pluginManager.registerEvents(Events(), plugin)
-        // val command = getCommand("command")
-        // command!!.setExecutor(Command())
-        // command.tabCompleter = TabCompleter()
+
+        // コマンド関係
+        val command = getCommand("backupmanager")
+        command!!.setExecutor(Command(plugin))
+        command.tabCompleter = TabCompleter()
     }
 }
--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/commands/Command.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/commands/Command.kt
@@ -1,12 +1,36 @@
 package com.github.ringoame196_s_mcPlugin.commands
 
+import com.github.ringoame196_s_mcPlugin.managers.BackupManager
+import org.bukkit.ChatColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
-import org.bukkit.event.Listener
+import org.bukkit.plugin.Plugin
 
-class Command:CommandExecutor {
+class Command(plugin: Plugin) : CommandExecutor {
+    private val backupManager = BackupManager(plugin)
+
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        TODO("Not yet implemented")
+        val subCommand = args[0]
+        when(subCommand) {
+            CommandConst.BACKUP_COMMAND -> backupCommand(sender)
+            else -> {
+                val message = "${ChatColor.RED}コマンド構文がありません"
+                sender.sendMessage(message)
+            }
+        }
+        return true
+    }
+
+    private fun backupCommand(sender: CommandSender) {
+        val commentaryStatus = backupManager.backupFolder()
+
+        if (commentaryStatus) {
+            val message = "${ChatColor.GOLD}正常にバックアップされました"
+            sender.sendMessage(message)
+        } else {
+            val message = "${ChatColor.RED}バックアップ中にエラーが起きました 詳細はコンソールをご覧ください"
+            sender.sendMessage(message)
+        }
     }
 }

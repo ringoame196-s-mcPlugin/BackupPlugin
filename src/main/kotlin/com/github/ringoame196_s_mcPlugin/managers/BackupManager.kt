b6324bc697853ec2063d9c032c0a4f09b491c55e
りんご飴196 <kazumac41@gmail.com>
2024-09-20 20:06:54 +0900

    [追加] バックアップ

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/managers/BackupManager.kt
@@ -0,0 +1,90 @@
+package com.github.ringoame196_s_mcPlugin.managers
+
+import org.bukkit.Bukkit
+import org.bukkit.plugin.Plugin
+import java.io.File
+import java.nio.file.FileVisitResult
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.StandardCopyOption
+import java.nio.file.attribute.BasicFileAttributes
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class BackupManager(private val plugin: Plugin) {
+    private val backupFolderPath = "${plugin.dataFolder}/backups"
+    private val worldFolder = Bukkit.getWorldContainer()
+
+    fun makeBackupFolder() {
+        if (!File(backupFolderPath).exists()) { // フォルダーが存在していなかったら
+            Files.createDirectory(Paths.get(backupFolderPath))
+        }
+    }
+
+    fun backupFolder():Boolean{
+        var normallyExecution = true // 正常に実行できたか
+        val config = plugin.config
+        val folderNames = config.getList("BackupFolderNames")?.toMutableList() ?: mutableListOf()
+        try {
+            for (folderName in folderNames) {
+                copyFolder(folderName.toString())
+            }
+        } catch (e:Exception) {
+            val errorMessage = e.message
+            println(errorMessage) // エラーメッセージを送信
+            normallyExecution = false
+        }
+        return normallyExecution
+    }
+
+    private fun copyFolder(folderName: String) {
+        val copySourcePath = "$worldFolder/$folderName"
+        val currentDateTime = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("MM-dd-HH-mm-ss")
+        val backupTimeFolderName = currentDateTime.format(formatter)
+        val backupFolderPath = Paths.get("$backupFolderPath/$backupTimeFolderName/$folderName")
+
+        makeTimeFolder(backupTimeFolderName) // 時間ごとのフォルダー作成
+
+        copyDir(Paths.get(copySourcePath), backupFolderPath)
+    }
+
+    private fun copyDir(source: Path, destination: Path) {
+        // コピー先のフォルダが存在しない場合は作成
+        if (Files.notExists(destination)) {
+            Files.createDirectories(destination)
+        }
+
+        // ファイルとフォルダを再帰的にコピー
+        Files.walkFileTree(
+            source,
+            object : SimpleFileVisitor<Path>() {
+                override fun preVisitDirectory(dir: Path, attrs: BasicFileAttributes): FileVisitResult {
+                    val targetPath = destination.resolve(source.relativize(dir))
+                    if (Files.notExists(targetPath)) {
+                        Files.createDirectory(targetPath)
+                    }
+                    return FileVisitResult.CONTINUE
+                }
+
+                override fun visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult {
+                    // session.lock ファイルをスキップ
+                    if (file.fileName.toString() == "session.lock") {
+                        return FileVisitResult.CONTINUE
+                    }
+                    Files.copy(file, destination.resolve(source.relativize(file)), StandardCopyOption.REPLACE_EXISTING)
+                    return FileVisitResult.CONTINUE
+                }
+            }
+        )
+    }
+
+    private fun makeTimeFolder(backupFolderName:String) {
+        val path = "$backupFolderPath/$backupFolderName"
+        if (!File(path).exists()) { // ファイルが無ければ作成
+            Files.createDirectory(Paths.get(path))
+        }
+    }
+}
